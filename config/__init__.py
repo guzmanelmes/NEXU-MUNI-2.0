a976078db5ad66bafc952739d8b51ad494c3e73c
Rodrigo Guzman Elmes <rguzman1@hotmail.cl>
2026-02-03 13:04:49 -0300

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,5 @@
+# config/__init__.py
+
+# Importamos la clase Config desde el archivo settings.py
+# Esto permite que "from config import Config" funcione correctamente
+from .settings import Config
